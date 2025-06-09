131183ed40e30ee9753b51b3d239d68caf836bd7
Pavlo Malysh <mal.pawlo@gmail.com>
2025-06-10 02:08:24 +0300

    add new

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,21 @@
 import { refs } from './refs.js';
 import { getArtists, getArtistsAlbumsId, getArtistById } from './artists-api.js';
-import { createdModal } from './render-function.js';
+import { createdModal, hideLoader, showLoader } from './render-function.js';
 
-const artistId = '65ada5b8af9f6d155db4806b';
+// const artistId = '65ada5b8af9f6d155db4806b';
 
-document.addEventListener('DOMContentLoaded', async () => {
+refs.artistsList.addEventListener('click', handlerModal);
+refs.closeModalBtn.addEventListener('click', handlerCloseModalClick);
+
+
+async function handlerModal(e) {
+  if (e.target.tagName !== 'BUTTON') return;
+
+  showLoader();
+  const artistId = e.target.dataset.artistId
+
   try {
-    const artistData = await getArtistById(artistId); 
+    const artistData = await getArtistById(artistId);
     const response = await getArtists();
 
     const artistsList = response.artists || [];
@@ -17,8 +26,28 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     const fullArtistData = { ...artistData, genres, albumsList };
     createdModal(fullArtistData);
+    hideLoader();
+    showModal();
 
   } catch (error) {
     console.error('Помилка при завантаженні артиста або альбомів:', error.message);
   }
-});
+
+}
+
+function handlerCloseModalClick(e) {
+  hideModal()
+}
+
+
+
+
+function showModal() {
+  refs.backdropModalElem.classList.add('is-open');
+  document.body.classList.add('modal-open');
+}
+
+function hideModal() {
+  refs.backdropModalElem.classList.remove('is-open');
+  document.body.classList.remove('modal-open')
+}
