9fce60e925b71dbbd8924ffd8f0bd692b2650e25
gaevuha <gaevuha@gmail.com>
2025-06-10 11:15:13 +0300

    update modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,53 +1,29 @@
-import { refs } from './refs.js';
-import { getArtists, getArtistsAlbumsId, getArtistById } from './artists-api.js';
-import { createdModal, hideLoader, showLoader } from './render-function.js';
 
-// const artistId = '65ada5b8af9f6d155db4806b';
+import { refs } from './refs.js';
+import {
+  handlerModal,
+  handlerCloseModalClick,
+  handlerBackdropClick,
+} from './handlers.js';
 
 refs.artistsList.addEventListener('click', handlerModal);
 refs.closeModalBtn.addEventListener('click', handlerCloseModalClick);
+refs.backdropModalElem.addEventListener('click', handlerBackdropClick);
 
-
-async function handlerModal(e) {
-  if (e.target.tagName !== 'BUTTON') return;
-
-  showLoader();
-  const artistId = e.target.dataset.artistId
-
-  try {
-    const artistData = await getArtistById(artistId);
-    const response = await getArtists();
-
-    const artistsList = response.artists || [];
-    const artistWithGenres = artistsList.find(artist => artist._id === artistId);
-    const genres = artistWithGenres?.genres || [];
-
-    const { albumsList = [] } = await getArtistsAlbumsId(artistId);
-
-    const fullArtistData = { ...artistData, genres, albumsList };
-    createdModal(fullArtistData);
-    hideLoader();
-    showModal();
-
-  } catch (error) {
-    console.error('Помилка при завантаженні артиста або альбомів:', error.message);
+function handleEscKey(e) {
+  if (e.key === 'Escape') {
+    hideModal();
   }
-
 }
 
-function handlerCloseModalClick(e) {
-  hideModal()
-}
-
-
-
-
-function showModal() {
+export function showModal() {
   refs.backdropModalElem.classList.add('is-open');
   document.body.classList.add('modal-open');
+  window.addEventListener('keydown', handleEscKey);
 }
 
-function hideModal() {
+export function hideModal() {
   refs.backdropModalElem.classList.remove('is-open');
-  document.body.classList.remove('modal-open')
+  document.body.classList.remove('modal-open');
+  window.removeEventListener('keydown', handleEscKey);
 }
